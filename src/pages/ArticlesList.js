2e017ac58722c8f6fa3b3818d8d6e761f776a098
Ketan Shukla <ketankshukla@gmail.com>
2021-01-16 22:10:18 -0800

    Added articles list to ArticleList component and converted each entry into a Link

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import articleContent from './article-content'
+import {Link} from 'react-router-dom'
 
 const ArticlesList = () => {
   return (
     <div>
       <h1>Articles</h1>
+      {articleContent.map((article) => (
+        <Link to={`/article/${article.name}`}>
+          <h3>{article.title}</h3>
+        </Link>
+      ))}
     </div>
-  )
+  );
 }
 
 export default ArticlesList
