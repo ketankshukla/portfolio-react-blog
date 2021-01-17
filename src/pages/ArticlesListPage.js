40774363c36c25e678e65bc9371d91b27429fa29
Ketan Shukla <ketankshukla@gmail.com>
2021-01-16 22:22:23 -0800

    Created reusable ArticlesList component and moved code from ArticleListPage

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,17 +1,12 @@
 import React from 'react'
 import articleContent from './article-content'
-import {Link} from 'react-router-dom'
+import ArticlesList from '../components/ArticlesList'
 
 const ArticlesListPage = () => {
   return (
     <div>
       <h1>Articles</h1>
-      {articleContent.map((article,key) => (
-        <Link className="article-list-item" key={key} to={`/article/${article.name}`}>
-          <h3>{article.title}</h3>
-          <p>{article.content[0].substring(0,150)}...</p>
-        </Link>
-      ))}
+      <ArticlesList articles={articleContent}/>
     </div>
   );
 }
