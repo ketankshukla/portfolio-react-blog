37cbf9daa4a6ac3da95d66abdf7533fae3a63f68
Ketan Shukla <ketankshukla@gmail.com>
2021-01-16 22:36:11 -0800

    Implemented the NotFoundPage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import "./App.css";
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
 import ArticlesListPage from "./pages/ArticlesListPage";
 import NavBar from "./NavBar";
+import NotFoundPage from "./pages/NotFoundPage";
 // comment
 const App = () => {
   return (
     <Router>
       <div className="App">
-      <NavBar/>
+        <NavBar />
         <div id="page-body">
-          <Route path="/" component={HomePage} exact />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/articles-list" component={ArticlesListPage} exact />
-          <Route path="/article/:name" component={ArticlePage} exact />
+          <Switch>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/articles-list" component={ArticlesListPage} exact />
+            <Route path="/article/:name" component={ArticlePage} exact />
+            <Route component={NotFoundPage} />
+          </Switch>
         </div>
       </div>
     </Router>
