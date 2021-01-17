bd2cacd568a7fb64c314670b436b2d7da5a9e655
Ketan Shukla <ketankshukla@gmail.com>
2021-01-16 21:43:07 -0800

    Added various pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import HomePage from './pages/HomePage';
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import "./App.css";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesList from "./pages/ArticlesList";
 // comment
 const App = () => {
   return (
     <Router>
       <div className="App">
-        <Route path="/" component = {HomePage} exact/>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/articles-list" component={ArticlesList} exact />
+        <Route path="/article" component={ArticlePage} exact />
       </div>
     </Router>
   );
-}
+};
 
 export default App;
